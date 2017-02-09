40d8800a877e138624f24e1ed9c24e59f85375d8
RasmusHaarslev <Rasmus.haarslev@gmail.com>
2017-02-09 11:17:53 +0100

    Adds magnus code to main

--- src/main/java/compiler/Main.java
+++ src/main/java/compiler/Main.java
@@ -2,10 +2,19 @@ package compiler;
 
 import org.bytedeco.javacpp.LLVM;
 
+import static org.bytedeco.javacpp.LLVM.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        LLVM.LLVMModuleRef moduleRef = ModuleLoader.loadModuleFromFile("../../c/hello.bc");
+        LLVMInitializeNativeAsmPrinter();
+        LLVMInitializeNativeAsmParser();
+        LLVMInitializeNativeDisassembler();
+        LLVMInitializeNativeTarget();
+
+        LLVMModuleRef moduleRef = ModuleLoader.loadModuleFromFile("../../c/hello.bc");
+
+        LLVMDumpModule(moduleRef);
     }
 }
