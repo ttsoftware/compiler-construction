87d278b4b2eef7d45bf0fc100c435d4d11e8b1ff
Gustav Jonsson <gustavj.dotzorx@gmail.com>
2017-05-06 18:41:15 +0200

    Fixed runtime error.

--- c++/src/VariableEntry.cpp
+++ c++/src/VariableEntry.cpp
@@ -2,47 +2,44 @@
 #include "llvm/IR/Constant.h"
 #include "llvm/IR/Instructions.h"
 #include "LatticeValue.hpp"
+#include "VariableEntry.hpp"
 
 
 using namespace llvm;
 
-class VariableEntry {
+/*bool isPointer;
+Value *self;
+Value *var;*/
 
-public:
+inline VariableEntry::VariableEntry(Value *var, bool isPointer) {
+    this->isPointer = isPointer;
+    this->var = var;
+}
 
-    bool isPointer;
-    Value *self;
-    Value *var;
+Value *VariableEntry::getVar() {
+    return this->var;
+}
 
-    inline VariableEntry(Value *var, bool isPointer) {
-        this->isPointer = isPointer;
-        this->var = var;
+bool VariableEntry::isPtr() {
+    return this->isPointer;
+}
+
+bool VariableEntry::isMaybeNull() {
+    return this->getVar() == (Value *) Enums::LatticeValue::MAYBE_NULL;
+}
+
+bool VariableEntry::equals(VariableEntry other) {
+    return this->var == other.getVar();
+}
+
+VariableEntry *VariableEntry::merge(VariableEntry *other) {
+    VariableEntry *ret = new VariableEntry(NULL, NULL);
+    if (other->getVar() == this->getVar()) {
+        ret->var = this->getVar();
+        ret->isPointer = other->isPtr();
+    } else {
+        ret->var = (Value *) Enums::LatticeValue::MAYBE_NULL;
     }
+    return ret;
+}
 
-    Value *getVar() {
-        return this->var;
-    }
-
-    bool isPtr() {
-        return this->isPointer;
-    }
-
-    bool isMaybeNull() {
-        return this->getVar() == (Value *) Enums::LatticeValue::MAYBE_NULL;
-    }
-
-    bool equals(VariableEntry other) {
-        return this->var == other.getVar();
-    }
-
-    VariableEntry* merge(VariableEntry* other) {
-        VariableEntry *ret = new VariableEntry(NULL, NULL);
-        if (other->getVar() == this->getVar()) {
-            ret->var = this->getVar();
-            ret->isPointer = other->isPtr();
-        } else {
-            ret->var = (Value *) Enums::LatticeValue::MAYBE_NULL;
-        }
-        return ret;
-    }
-};
